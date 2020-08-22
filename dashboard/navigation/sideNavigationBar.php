a1e4c9066d86d7bed47bccfdb9d158e8b29bc0ea
mohammed fayez <fayezmohammed23@gmail.com>
2020-08-22 10:20:19 +0530

    navigation added

--- dashboard/navigation/sideNavigationBar.php
+++ dashboard/navigation/sideNavigationBar.php
@@ -13,13 +13,26 @@ function sideNavigationBar()
         </svg>
         Dashboard
     </div>
-    <div class="pl-4 mt-4">
+    <div class="pl-4 mt-4 menuOptionBar">
         <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-book-fill mr-2" fill="currentColor"
             xmlns="http://www.w3.org/2000/svg">
             <path
                 d="M15.261 13.666c.345.14.739-.105.739-.477V2.5a.472.472 0 0 0-.277-.437c-1.126-.503-5.42-2.19-7.723.129C5.696-.125 1.403 1.56.277 2.063A.472.472 0 0 0 0 2.502V13.19c0 .372.394.618.739.477C2.738 12.852 6.125 12.113 8 14c1.875-1.887 5.262-1.148 7.261-.334z" />
         </svg>
         Programs
+        <div class="innerMenu">
+            <a href="http://">
+                <p>
+                    Programs
+                </p>
+            </a>
+           
+            <hr/>
+            <p class="">
+                Course
+            </p>
+        </div>
+
     </div>
     <div class="pl-4 mt-4">
         <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-bounding-box-circles mr-2"
