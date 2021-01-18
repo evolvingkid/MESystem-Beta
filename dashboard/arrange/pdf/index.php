73b2ea371d49a5d67dae7251a838bf1eec2e3ff5
fayez <fayezmohammed23@gmail.com>
2021-01-18 22:33:05 +0530

    pdf to js

---
+++ dashboard/arrange/pdf/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../../assets/CSS/custom/pdf.css" />
+    <script src="../../../assets/JS/custom/pdf.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.js"></script>
+
+</head>
+
+<body>
+    <div class="container d-flex justify-content-center mt-50 mb-50">
+        <div class="row">
+            <div class="col-md-12 text-right mb-3">
+                <button class="btn btn-primary" id="download"> download pdf</button>
+            </div>
+            <div class="col-md-12">
+                <div class="card" id="invoice">
+                    <div class="card-header bg-transparent header-elements-inline">
+                        <h6 class="card-title text-primary"> Exam Seats </h6>
+                    </div>
+                    
+                    <div class="table-responsive">
+                        <table class="table table-lg">
+                            <thead>
+                                <tr>
+                                    <th>Exam Hall</th>
+                                    <th>name</th>
+                                   
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                            <tr>
+                                <td> 101</td>
+                                <td> fayez</td>
+
+                            </tr>
+                                
+                            </tbody>
+                        </table>
+                    </div>
+                   
+                    </div>
+                    <div class="card-footer"> <span class="text-muted">Lorem ipsum dolor sit amet, consectetur
+                            adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+                            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                            fugiat nulla pariatur.Duis aute irure dolor in reprehenderit</span> </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
